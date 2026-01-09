6de17ee0baad61a90fef3db540015d0757bfb120
krijan maharjan <krijanmaharjan31@gmail.com>
2026-01-09 22:19:05 +0545

    first stage

---
+++ python1.py
@@ -0,0 +1,34 @@
+
+money1=int(input("enter the number"))
+money2=int(input("enter second number"))
+name=input("enter the persion name")
+name2=input("ente the second persion name ")
+
+print("the total amount of ",name,"and",name2,"is RS",money1+money2)
+
+#list 
+student=["krijan",15,"male"]
+print(student)
+
+#tupple
+students=("krijan","ashish","ompandey")
+print (student)
+
+#dictionary
+family_members=[
+    {
+        "father":"xyz",
+        "age":"45",
+
+    },
+    {
+        "mother":"abc",
+        "age":"40"
+    }
+]
+print(family_members[0])
+print(family_members[1])
+
+
+
+

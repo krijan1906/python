af1c7df5265a536d7b3f7f5ca74e3c759f85ac02
krijan maharjan <krijanmaharjan31@gmail.com>
2026-01-11 23:30:54 +0545

    loop

---
+++ day3/forloop/forloop.py
@@ -0,0 +1,22 @@
+persional_details=[
+    {
+        "name":"krijanmaharjan",
+        "age":20,
+        "gmail":"krijanmaharjan31@gmail.com",
+    },
+    {
+        "name":"ashish",
+        "age":90,
+        "gmail":"ashish@gmail.com",
+    },
+    {
+        "name":"om kumar pandey",
+        "age":28,
+        "gmail":"om@gmail.com",
+    },
+]
+number=len(persional_details)
+
+print(number)
+for person in range(number):
+    print(persional_details[person])
